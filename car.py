e889332ecce75b73775feb9df7e7ca89f51c62f9
Awuor Marvin <awuor.marvin5@gmail.com>
2017-06-13 10:52:45 -0700

    Created the car class and added the constructor

---
+++ car.py
@@ -0,0 +1,7 @@
+class Car(object):
+    
+    def __init__(self, car_type="saloon", name="General", model="GM", speed=0):
+        self.car_type = car_type
+        self.name = name
+        self.model = model
+        self.speed = speed
