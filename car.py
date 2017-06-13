17a6203049dc0e735a828637e62a061f13b93534
Awuor Marvin <awuor.marvin5@gmail.com>
2017-06-13 11:25:03 -0700

    Added the conditions for num_of_doors and num_of_wheels

--- car.py
+++ car.py
@@ -1,10 +1,20 @@
 class Car(object):
     
-    def __init__(self, car_type="saloon", name="General", model="GM", speed=0):
+    def __init__(self, name="General", model="GM",car_type="saloon", speed=0):
         self.car_type = car_type
         self.name = name
         self.model = model
         self.speed = speed
+        self.num_of_wheels = 4
+
+        if self.name == "Porsche" or self.name == "Koenigsegg":
+            self.num_of_doors = 2
+        else:
+            self.num_of_doors = 4
+
+        if self.car_type == "trailer":
+            self.num_of_wheels = 8
+            self.speed = 0
 
     def is_saloon(self):
         if self.car_type != "trailer":
